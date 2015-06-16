942b9efb7ee2985c49ed40dea33b2f96565b62b2
Tom Hughes <thomas.peter.hughes@gmail.com>
2015-06-16 10:54:49 +0800

    production added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,13 @@ Myflix::Application.configure do
   config.action_mailer.default_url_options = { :host => 'mailertest.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'gmail.com',
-    :user_name => ENV['gmail_username'],
-    :password => ENV['gmail_password'],
-    :authentication => 'plain',
-    :enable_starttls_auto => true
-  }
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
+}
 end
 
